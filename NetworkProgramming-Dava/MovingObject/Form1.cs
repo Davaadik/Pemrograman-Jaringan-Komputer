9f4df55be1ab2c4f5e216f9fa6f68d2e36a65104
02_Mochammad Dava Adi Kurniawan <111889786+Davaadik@users.noreply.github.com>
2024-09-11 23:18:00 +0700

    Pengumpulan tugas Pemrograman Jaringan Komputer

---
+++ NetworkProgramming-Dava/MovingObject/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MovingObject
+{
+    public partial class Form1 : Form
+    {
+        Pen red = new Pen(Color.Red);
+        Rectangle rect = new Rectangle(20, 20, 30, 30);
+        SolidBrush fillBlue = new SolidBrush(Color.Blue);
+        int slide = 10;
+        TcpListener server;
+        Thread listenerThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 50;
+            timer1.Enabled = true;
+
+            // Start the server
+            listenerThread = new Thread(StartServer);
+            listenerThread.Start();
+        }
+
+        private void StartServer()
+        {
+            server = new TcpListener(IPAddress.Any, 8888);
+            server.Start();
+
+            while (true)
+            {
+                try
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    Thread clientThread = new Thread(() => HandleClient(client));
+                    clientThread.Start();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void HandleClient(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            while (true)
+            {
+                try
+                {
+                    string message = $"{rect.X},{rect.Y}";
+                    byte[] data = Encoding.ASCII.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+                    Thread.Sleep(50); // Sync with the timer
+                }
+                catch (Exception)
+                {
+                    client.Close();
+                    break;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MoveRectangle();
+            Invalidate();
+        }
+
+        private void MoveRectangle()
+        {
+            if (rect.X >= this.Width - rect.Width * 2)
+                slide = -10;
+            else if (rect.X <= rect.Width / 2)
+                slide = 10;
+
+            rect.X += slide;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawRectangle(red, rect);
+            g.FillRectangle(fillBlue, rect);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            server.Stop();
+            listenerThread.Abort();
+            base.OnFormClosing(e);
+        }
+    }
+}
---
+++ NetworkProgramming-Dava/MovingObjectClient/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MovingObjectClient
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        private Thread receiveThread;
+        private Rectangle rect;
+        private Pen red = new Pen(Color.Red);
+        private SolidBrush fillBlue = new SolidBrush(Color.Blue);
+
+        public Form1()
+        {
+            InitializeComponent();
+            rect = new Rectangle(20, 20, 30, 30); // Inisialisasi ukuran kotak
+            ConnectToServer();
+        }
+
+        // Method untuk koneksi ke server
+        private void ConnectToServer()
+        {
+            try
+            {
+                client = new TcpClient("127.0.0.1", 8888); // Port should match the server's port
+                stream = client.GetStream();
+
+                // Membuat thread untuk menerima data dari server
+                receiveThread = new Thread(new ThreadStart(ReceiveData));
+                receiveThread.IsBackground = true;
+                receiveThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error connecting to server: " + ex.Message);
+            }
+        }
+
+        // Method untuk menerima data dari server
+        private void ReceiveData()
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                    // Parsing posisi dari string yang diterima
+                    string[] position = data.Split(',');
+                    if (position.Length == 2)
+                    {
+                        int x = int.Parse(position[0]);
+                        int y = int.Parse(position[1]);
+
+                        // Update posisi kotak
+                        rect.X = x;
+                        rect.Y = y;
+
+                        // Force refresh form untuk menggambar ulang
+                        this.Invalidate();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error receiving data: " + ex.Message);
+            }
+        }
+
+        // Method untuk menggambar kotak
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawRectangle(red, rect);
+            g.FillRectangle(fillBlue, rect);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Menutup koneksi saat form ditutup
+            if (client != null)
+            {
+                client.Close();
+            }
+            if (receiveThread != null)
+            {
+                receiveThread.Abort();
+            }
+        }
+    }
+}
